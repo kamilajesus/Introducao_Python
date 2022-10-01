2693baa0cafa48f353864474ede71f4b8e112605
Kamila Jesus <kamilaalves727@gmail.com>
2022-10-01 00:47:16 -0300

    New Commit

--- forca.py
+++ forca.py
@@ -1,30 +1,55 @@
-def jogar():
-    print("********************************")
-    print("***Bem vindo ao jogo da forca***")
-    print("********************************")
+import random
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_","_"]
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    arquivo = open("/home/kamilajesus/PycharmProjects/jogos/list.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     
     enforcou = False
     acertou = False
+    erros = 0
     
     print(letras_acertadas)
 
     while not enforcou and not acertou:
         
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
-        
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
-    print("Fim do jogo")
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
+    print("Fim do Jogo")
+        
 
 if(__name__ == "__main__"):
     jogar()
